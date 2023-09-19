428319f2fa4e5255258be3b3d1ca97b0ccff20c6
Bradley-Roberts <rob19009@byui.edu>
2023-09-19 14:21:25 -0700

    Just pushing my projects to github

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+                string GradePercentageinput = Console.ReadLine();
+                int number = int.Parse(GradePercentageinput);
+                string Gradeletter = "";
+                if (number >= 90)
+                {
+                    Gradeletter = "A";
+                }
+                else if (number >= 80)
+                {
+                    Gradeletter = "B";
+                }
+                else if (number >= 70)
+                {
+                    Gradeletter = "C";
+                }
+                else if (number >= 60)
+                {
+                    Gradeletter = "D";
+                }
+                else if (number < 60)
+                {
+                    Gradeletter = "F";
+                }
+                    Console.WriteLine($"Your letter grade is {Gradeletter}");
+                if (number < 70)
+                {
+                    Console.WriteLine("You unfortunately are failing this course. Keep trying and eventually you will get it");
+                }
+
     }
+
+
 }
