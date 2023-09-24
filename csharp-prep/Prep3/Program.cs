1839e11fa50031d91989cc395202f1424d4e5631
Bradley-Roberts <rob19009@byui.edu>
2023-09-24 13:32:55 -0600

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicnumber = randomGenerator.Next(1, 100);
+        
+        int responsenumber = 0;
+        while (responsenumber != magicnumber)
+            {
+            Console.WriteLine("What is your guess?");
+            string guessresponse = Console.ReadLine();
+                responsenumber = int.Parse(guessresponse);
+
+
+                if (responsenumber > magicnumber)
+                    {Console.WriteLine("Lower");}
+                else if (responsenumber < magicnumber)
+                    {Console.WriteLine("Higher");}
+                else if (responsenumber == magicnumber)
+                    {Console.WriteLine("You have guessed the magic number!");}
+        }
+
     }
 }
